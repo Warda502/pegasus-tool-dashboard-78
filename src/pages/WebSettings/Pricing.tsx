686e5b4c18af2735163528d64765fcba5885b633
Warda Khaild <wrdhkhald2004@gmail.com>
2025-05-06 22:39:52 +0300

    Update Pricing.tsx

--- src/pages/WebSettings/Pricing.tsx
+++ src/pages/WebSettings/Pricing.tsx
@@ -217,7 +217,7 @@ export default function Pricing() {
             setIsAddDialogOpen(true);
           }}>
             <Plus className="mr-2 h-4 w-4" />
-            {t("addNewPlan") || "addNewPlan"}
+            {t("addNewPlan") || "Add New Plan"}
           </Button>
         </CardHeader>
         <CardContent>
